8f2045bc9c7a3f4edacd43c8821a0a52c4a13e3b
Surajudeen Akande <sirolad@gmail.com>
2019-05-17 14:51:04 +0100

    add spinner

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,12 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
+import { Redirect } from "react-router-dom";
+import Spinner from "../common/Spinner";
 
-class CoursesPage extends Component {
+class CoursesPage extends React.Component {
+  state = {
+    redirectToAddCoursePage: false
+  };
+
   componentDidMount() {
     const { courses, authors, actions } = this.props;
 
@@ -26,8 +32,25 @@ class CoursesPage extends Component {
   render() {
     return (
       <>
+        {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
-        <CourseList courses={this.props.courses} />
+        {this.props.loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <button
+              style={{ marginBottom: 20 }}
+              className="btn btn-primary add-course"
+              onClick={() =>
+                this.setState({ redirectToAddCoursePage: true })
+              }
+            >
+              Add Course
+            </button>
+
+            <CourseList courses={this.props.courses} />
+          </>
+        )}
       </>
     );
   }
@@ -36,7 +59,8 @@ class CoursesPage extends Component {
 CoursesPage.propTypes = {
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  actions: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
 function mapStateToProps(state) {
@@ -50,7 +74,8 @@ function mapStateToProps(state) {
               authorName: state.authors.find(a => a.id === course.authorId).name
             };
           }),
-    authors: state.authors
+    authors: state.authors,
+    loading: state.apiCallsInProgress > 0
   };
 }
 
@@ -67,3 +92,4 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(CoursesPage);
+ 
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,16 +1,23 @@
 import * as types from "./actionTypes";
 import * as courseApi from '../../api/courseApi'
-
-export function createCourse(course) {
-    return { type: types.CREATE_COURSE, course }
-}
+import { beginApiCall } from './apiStatusActions'
 
 export function loadCourseSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+
 export function loadCourses() {
   return function(dispatch) {
+    dispatch(beginApiCall())
     return courseApi
       .getCourses()
       .then(courses => {
@@ -21,3 +28,21 @@ export function loadCourses() {
       });
   };
 }
+
+export function saveCourse(course) {
+  //eslint-disable-next-line no-unused-vars
+  return function(dispatch, getState) {
+    dispatch(beginApiCall());
+    return courseApi
+      .saveCourse(course)
+      .then(savedCourse => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
+}
+
